1a7b10a708febff076bcf9b097d53fc2e6e3a5b3
Ganesh16896 <ramkrishna18547@gmail.com>
2025-01-22 22:29:35 +0530

    done seo

---
+++ src/app/sitemap.js
@@ -0,0 +1,12 @@
+export default async function sitemap() {
+  const dataX = [
+    {
+      url: "https://seyantek.com",
+      lastModified: new Date(),
+      changeFrequency: "yearly",
+      priority: 1,
+    },
+  ];
+
+  return dataX;
+}
